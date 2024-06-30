037677dbf80a1e6cc62f4a328b77718e60e43b1a
Minonono <clock2elizabeth@gmail.com>
2024-07-01 00:04:01 +0900

    amend! modify hoge::main, delete arg optionnal
    modify hoge::main, delete arg

--- commit-fixup/hoge.py
+++ commit-fixup/hoge.py
@@ -11,11 +11,6 @@ def goodbye_world(name: str):
 
 def main():
     name = "world"
-    if len(sys.argv) < 1:
-        print("Usage: python hoge.py [name]")
-        raise SystemExit(1)
-    else:
-        name = sys.argv[1]
 
     hello_world(name)
     goodbye_world(name)
