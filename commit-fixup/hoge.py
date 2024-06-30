ad3286017bee6bc7e0a1caaf1f5c88a9dbe49e6b
Minonono <clock2elizabeth@gmail.com>
2024-06-30 22:16:20 +0900

    add hoge.py

---
+++ commit-fixup/hoge.py
@@ -0,0 +1,15 @@
+def hello_world():
+    print("Hello, world!")
+
+
+def goodbye_world():
+    print("Goodbye, world!")
+
+
+def main():
+    hello_world()
+    goodbye_world()
+
+
+if __name__ == "__main__":
+    main()
